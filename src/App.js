ae64c3d38a31fc825805e6467245425e6dfd7cdc
Utkarsh <utkarshbankar07@gmail.com>
2024-02-28 19:05:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,177 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="logo">
+      <img src="./asserts/logo.png" alt="Logo" />
     </div>
+    <div className="content">
+      <img src="./asserts/1.png" alt="Award Pic" />
+      <div className="descp">
+        <h3>
+          <strong>
+            C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for
+            the 4th time.
+          </strong>
+        </h3>
+        <ul>
+          <li>
+            C.R.I.'s energy efficient products are well recognized by various
+            Government Institutions, as trustworthy products for various
+            projects across the globe to save energy.
+          </li>
+          <li>
+            C.R.I. is the highest contributor in the country for the projects of
+            EESL (Energy Efficiency Services Limited) to replace the old
+            inefficient pumps with 5 Star rated energy efficient smart pumps
+            with IoT enabled control panel.
+          </li>
+        </ul>
+        <img src="./asserts/2.png" alt="Award Receiving photo" />
+        <p>
+          Government of India has awarded the{" "}
+          <strong>"National Energy Conservation Award 2018"</strong>. Mr. G.
+          Selvaraj, Joint Managing Director of C.R.I. Group received the award
+          from Smt. Sumitra Mahajan, Speaker of Lok Sabha &amp; Shri. Raj Kumar
+          Singh, Honorable Minister of State.
+        </p>
+      </div>
+    </div>
+    <p>
+      INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING
+      IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE
+      NATION.
+    </p>
+    <div className="pumps-sec">
+      <img src="./asserts/3.png" alt="Pumps" />
+      <p>
+        Valves - Pumps - Pipes - IoT Drives &amp; Controllers - Wires &amp;
+        Cables - Solar Systems - Motors
+      </p>
+    </div>
+    <hr />
+    <div className="services">
+      <h3>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h3>
+      <div>
+        <span>
+          CHEMICALS &amp; PROCESS
+          <span className="ver_line" />
+        </span>
+        <span>
+          POWER
+          <span className="ver_line" />
+        </span>
+        <span>
+          WATER &amp; WASTE
+          <span className="ver_line" />
+        </span>
+        <span>
+          WATER OILS &amp; GAS
+          <span className="ver_line" />
+        </span>
+        <span>
+          PHARMA
+          <span className="ver_line" />
+        </span>
+        <span>
+          SUGARS &amp; DISTILLERIES
+          <span className="ver_line" />
+        </span>
+        <span>
+          PAPER &amp; PULP
+          <span className="ver_line" />
+        </span>
+        <span>
+          MARINE &amp; DEFENCE
+          <span className="ver_line" />
+        </span>
+        <span>
+          METAL &amp; MINING
+          <span className="ver_line" />
+        </span>
+        <span>
+          FOOD &amp; BEVERAGE
+          <span className="ver_line" />
+        </span>
+        <span>
+          PETROCHEMICAL &amp; REFINERIES
+          <span className="ver_line" />
+        </span>
+        <span>
+          SOLAR
+          <span className="ver_line" />
+        </span>
+        <span>
+          BUILDING
+          <span className="ver_line" />
+        </span>
+        <span>
+          HVAC
+          <span className="ver_line" />
+        </span>
+        <span>
+          FIRE FIGHTING
+          <span className="ver_line" />
+        </span>
+        <span>AGRICULTURE &amp; RESIDENTIAL.</span>
+      </div>
+    </div>
+    <div className="footer">
+      <div>
+        <h4>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={16}
+            height={16}
+            fill="currentColor"
+            className="bi bi-telephone-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
+            />
+          </svg>
+          Toll free <a href='#'>1800 200 1234</a>
+        </h4>
+      </div>
+      <div>
+        <h4>
+          <a href='#'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={16}
+              height={16}
+              fill="currentColor"
+              className="bi bi-facebook"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+            </svg>
+            www.facebook.com/cricpumps
+          </a>
+        </h4>
+      </div>
+      <div>
+        <h4>
+          <a href='#'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={16}
+              height={16}
+              fill="currentColor"
+              className="bi bi-globe2"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855q-.215.403-.395.872c.705.157 1.472.257 2.282.287zM4.249 3.539q.214-.577.481-1.078a7 7 0 0 1 .597-.933A7 7 0 0 0 3.051 3.05q.544.277 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9 9 0 0 1-1.565-.667A6.96 6.96 0 0 0 1.018 7.5zm1.4-2.741a12.3 12.3 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332M8.5 5.09V7.5h2.99a12.3 12.3 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.6 13.6 0 0 1 7.5 10.91V8.5zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741zm-3.282 3.696q.18.469.395.872c.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a7 7 0 0 1-.598-.933 9 9 0 0 1-.481-1.079 8.4 8.4 0 0 0-1.198.49 7 7 0 0 0 2.276 1.522zm-1.383-2.964A13.4 13.4 0 0 1 3.508 8.5h-2.49a6.96 6.96 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667m6.728 2.964a7 7 0 0 0 2.275-1.521 8.4 8.4 0 0 0-1.197-.49 9 9 0 0 1-.481 1.078 7 7 0 0 1-.597.933M8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855q.216-.403.395-.872A12.6 12.6 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.96 6.96 0 0 0 14.982 8.5h-2.49a13.4 13.4 0 0 1-.437 3.008M14.982 7.5a6.96 6.96 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008zM11.27 2.461q.266.502.482 1.078a8.4 8.4 0 0 0 1.196-.49 7 7 0 0 0-2.275-1.52c.218.283.418.597.597.932m-.488 1.343a8 8 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z" />
+            </svg>
+            www.crigroups.com
+          </a>
+        </h4>
+      </div>
+    </div>
+  </div>
   );
 }
 
